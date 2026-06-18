ce9b7ad21ad49378a0dcff3beecce578380ec1a1
TomaTheLoser <gtkgaming6236@gmail.com>
2026-06-18 19:11:10 +0700

    Add files via upload

--- 6_buildjdk.sh
+++ 6_buildjdk.sh
@@ -72,7 +72,7 @@ else
     --with-freetype=bundled \
     "
   AUTOCONF_x11arg="--with-x=/opt/X11/include/X11 --prefix=/usr/lib"
-  sameflags="-arch arm64 -DHEADLESS=1 -I$PWD/ios-missing-include -Wno-implicit-function-declaration -DTARGET_OS_OSX"
+  sameflags="-arch arm64 -DHEADLESS=1 -I$PWD/ios-missing-include -Wno-implicit-function-declaration -Wno-error=implicit-function-declaration -DTARGET_OS_OSX"
   export CFLAGS+=" $sameflags"
   export LDFLAGS+="-arch arm64"
   export BUILD_SYSROOT_CFLAGS="-isysroot ${themacsysroot}"
